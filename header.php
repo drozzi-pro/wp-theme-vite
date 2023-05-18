81d3f672b86bcb5a31c146cba9a87f2d85022901
kviron <kviron.web.dev@gmail.com>
2023-05-18 21:35:13 +0300

    Добавил папки и 404 страницу

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="app">
+    <?php get_template_part('templates/layouts/header') ?>
